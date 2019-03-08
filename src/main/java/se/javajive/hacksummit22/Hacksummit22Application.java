6543dc6ac7a12e279538e5427477337c7c9b2301
Helena Hjertén <helena@HelenasMBP2015.local>
2019-03-08 15:53:56 +0100

    first commit

---
+++ src/main/java/se/javajive/hacksummit22/Hacksummit22Application.java
@@ -0,0 +1,13 @@
+package se.javajive.hacksummit22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hacksummit22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hacksummit22Application.class, args);
+	}
+
+}
